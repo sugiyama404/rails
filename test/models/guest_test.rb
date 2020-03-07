cf349d557519a56c88c88aab38cb62f6bfc2f6df
a05101 <sugiyamakouji@Air.local>
2020-03-07 22:22:17 +0900

    ,

--- test/models/guest_test.rb
+++ test/models/guest_test.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'minitest/autorun'
+
 class GuestTest < ActiveSupport::TestCase
   def setup
     @guests = Guest.new(name: 'Example User', email: 'user@example.com', password_digest: 'pass')
