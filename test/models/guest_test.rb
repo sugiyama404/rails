e0a6efa84ca27d8797e8b4bccb803e37199258c5
a05101 <sugiyamakouji@Air.local>
2020-03-15 13:13:33 +0900

    test

--- test/models/guest_test.rb
+++ test/models/guest_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require '/sampleApp/test/test_helper'
+require_relative './test_helper'
+# require 'test_helper'
 
 class GuestTest < ActiveSupport::TestCase
   def setup
