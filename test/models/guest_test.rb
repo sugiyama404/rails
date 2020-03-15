9a28ee9393292a065e47b6441570108fc0bf734e
a05101 <sugiyamakouji@Air.local>
2020-03-15 14:20:25 +0900

    d

--- test/models/guest_test.rb
+++ test/models/guest_test.rb
@@ -5,7 +5,7 @@ require '/home/circleci/sampleApp/test/test_helper'
 
 class GuestTest < ActiveSupport::TestCase
   def setup
-    @guests = Guest.new(name: 'Example User', email: 'user@example.com', password_digest: 'pass')
+    @guests = Guest.new(name: 'Example User', email: 'user@example.com', password_digest: '$2a$12$1x.Cb8Tb3vR5SLt/1edWteuWhoRIJukaIOk4Gqaq7LvEJFD7B7kl.')
   end
   test 'should be valid' do
     assert @guests.valid?
