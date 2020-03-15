2576629316a6018ede14168d05e5b5076c27dd69
a05101 <sugiyamakouji@Air.local>
2020-03-15 13:03:53 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'tops#top'
 
+  post '/users', to: 'users#user'
+  # あとで消す
   get '/users', to: 'users#user'
   get '/users/:id/login', to: 'login#login'
   post '/users/:id/login', to: 'login#check'
--- test/models/guest_test.rb
+++ test/models/guest_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require '/sampleApp/test/test_helper'
 
 class GuestTest < ActiveSupport::TestCase
   def setup
