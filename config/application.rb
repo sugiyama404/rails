b88f1b28a7dba0a00d1900d8f370352d6acc2f93
a05101 <sugiyamakouji@Air.local>
2020-04-21 12:59:41 +0900

    test

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SampleApp
+module AppName
   # module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
