990e0354764540cedd6305e1c65296bd68a701b6
a05101 <sugiyamakouji@Air.local>
2020-04-27 16:32:48 +0900

    1

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -7,7 +7,7 @@ class TopsController < ApplicationController
     # ダミーデータ
     boo = Hotelday.order(days: :desc).limit(1)
     boo2 = boo.pluck(:days)
-    miniday2 = boo2[0] + 1
+    miniday2 = boo2[0]
     # miniday2 = if miniday <= Date.today
     #             Date.today
     #           else
@@ -15,9 +15,9 @@ class TopsController < ApplicationController
     #          end
 
     (1..15).each do |num|
-      hoteldays = Hotelday.new(hotel_id: num, bestvacant: rand(4), richvacant: rand(6), fourthvacant: rand(10), doublevacant: rand(10), days: miniday2)
+      hoteldays = Hotelday.new(hotel_id: num, bestvacant: rand(4), richvacant: rand(6), fourthvacant: rand(10), doublevacant: rand(10), days: miniday2.tomorrow)
       hoteldays.save
-      hotelprices = Hotelprice.new(hotel_id: num, bestprice: (rand(9) * 1000 + 40_000), richprice: (rand(9) * 1000 + 30_000), fourthprice: (rand(9) * 1000 + 20_000), doubleprice: (rand(9) * 1000 + 10_000), newday: miniday2)
+      hotelprices = Hotelprice.new(hotel_id: num, bestprice: (rand(9) * 1000 + 40_000), richprice: (rand(9) * 1000 + 30_000), fourthprice: (rand(9) * 1000 + 20_000), doubleprice: (rand(9) * 1000 + 10_000), newday: miniday2.tomorrow)
       hotelprices.save
     end
   end
