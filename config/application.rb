2b1317699e496770af95996f021253a2b2526755
a05101 <sugiyamakouji@Air.local>
2020-04-29 22:28:31 +0900

    管理者画面の日本語化

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,9 @@ module AppName
     config.load_defaults 5.2
 
     config.time_zone = 'Tokyo'
-    # 　コメント増やす
+
     # デフォルトのlocaleを日本語(:ja)にする
-    # config.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
