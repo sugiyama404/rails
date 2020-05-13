b6d9beebfc808396f041096118cb4c95cd8cc945
a05101 <sugiyamakouji@Air.local>
2020-05-13 16:33:44 +0900

    retry i create my view migration.

---
+++ db/migrate/20200513073116_create_my_view.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class CreateMyView < ActiveRecord::Migration[5.2]
+  def change
+    execute <<-SQL
+      drop view if exists confirms
+    SQL
+
+    execute <<~SQL
+            CREATE OR REPLACE VIEW confirms AS
+            SELECT
+          `t1`.`id` AS `id`,
+          `t1`.`name` AS `name`,
+          `t1`.`address` AS `address`,
+          `t1`.`imgname` AS `imgname`,
+          `t1`.`bestname` AS `bestname`,
+          `t1`.`richname` AS `richname`,
+          `t1`.`fourthname` AS `fourthname`,
+          `t1`.`doublename` AS `doublename`,
+          `t2`.`bestvacant` AS `bestvacant`,
+          `t2`.`richvacant` AS `richvacant`,
+          `t2`.`fourthvacant` AS `fourthvacant`,
+          `t2`.`doublevacant` AS `doublevacant`,
+          `t2`.`days` AS `days`,
+          `t3`.`bestprice` AS `bestprice`,
+          `t3`.`richprice` AS `richprice`,
+          `t3`.`fourthprice` AS `fourthprice`,
+          `t3`.`doubleprice` AS `doubleprice`
+      from
+          ((`railsdatabase`.`hotels` `t1`
+      left join `railsdatabase`.`hoteldays` `t2` on
+          ((`t1`.`id` = `t2`.`hotel_id`)))
+      left join `railsdatabase`.`hotelprices` `t3` on
+          ((`t1`.`id` = `t3`.`hotel_id`)))
+      where
+          (`t2`.`days` > (curdate() - interval 1 day));
+    SQL
+  end
+end
