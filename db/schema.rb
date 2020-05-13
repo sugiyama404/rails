66f39847bf42c88e54cb439c48f855179f7abc87
a05101 <sugiyamakouji@Air.local>
2020-05-13 15:09:42 +0900

    buck

--- db/schema.rb
+++ db/schema.rb
@@ -104,34 +104,4 @@ ActiveRecord::Schema.define(version: 20_200_425_045_842) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
-  create_view 'confirms', sql_definition: <<~SQL
-      CREATE VIEW  `railsdatabase`.`confirms` AS
-    select
-      `t1`.`id` AS `id`,
-      `t1`.`name` AS `name`,
-      `t1`.`address` AS `address`,
-      `t1`.`imgname` AS `imgname`,
-      `t1`.`bestname` AS `bestname`,
-      `t1`.`richname` AS `richname`,
-      `t1`.`fourthname` AS `fourthname`,
-      `t1`.`doublename` AS `doublename`,
-      `t2`.`bestvacant` AS `bestvacant`,
-      `t2`.`richvacant` AS `richvacant`,
-      `t2`.`fourthvacant` AS `fourthvacant`,
-      `t2`.`doublevacant` AS `doublevacant`,
-      `t2`.`days` AS `days`,
-      `t3`.`bestprice` AS `bestprice`,
-      `t3`.`richprice` AS `richprice`,
-      `t3`.`fourthprice` AS `fourthprice`,
-      `t3`.`doubleprice` AS `doubleprice`
-    from
-      ((`railsdatabase`.`hotels` `t1`
-    left join `railsdatabase`.`hoteldays` `t2` on
-      ((`t1`.`id` = `t2`.`hotel_id`)))
-    left join `railsdatabase`.`hotelprices` `t3` on
-      ((`t1`.`id` = `t3`.`hotel_id`)))
-    where
-      (`t2`.`days` > (curdate() - interval 1 day));
-  SQL
 end
