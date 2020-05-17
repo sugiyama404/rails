a6671a57423c6bf266189a3b3c3aee62b4ccb278
a05101 <sugiyamakouji@Air.local>
2020-05-17 11:49:20 +0900

    dockerfile fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_05_13_073116) do
 
-  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_073116) do
     t.index ["email"], name: "index_admin_users_on_email", unique: true
   end
 
-  create_table "guests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "guests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "mail"
     t.string "password_digest"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_073116) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "hoteldays", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "hoteldays", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "hotel_id"
     t.integer "bestvacant"
     t.integer "richvacant"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_073116) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "hotelprices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "hotelprices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "hotel_id"
     t.integer "bestprice"
     t.integer "richprice"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_073116) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "hotels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "hotels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "bestname"
     t.string "richname"
@@ -66,14 +66,14 @@ ActiveRecord::Schema.define(version: 2020_05_13_073116) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "reserveds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "reserveds", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "guest_id"
     t.integer "hotel_id"
     t.integer "stayday"
@@ -84,13 +84,13 @@ ActiveRecord::Schema.define(version: 2020_05_13_073116) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
