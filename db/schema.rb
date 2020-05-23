9b6f47d5c4d974361400d4f50c633497cbd76253
a05101 <sugiyamakouji@Air.local>
2020-05-23 19:53:49 +0900

    crear local problem.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_073116) do
+ActiveRecord::Schema.define(version: 2020_05_23_073635) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -104,4 +104,3 @@ ActiveRecord::Schema.define(version: 2020_05_13_073116) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-end
